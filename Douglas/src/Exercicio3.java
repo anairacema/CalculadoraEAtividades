fdbacb5b467df5328babcda27740032b83dedaa5
Ana Iracema <nana.iracema@gmail.com>
2023-03-16 19:13:53 -0300

    Exercicio 3 realizado

--- Douglas/src/Exercicio3.java
+++ Douglas/src/Exercicio3.java
@@ -5,9 +5,23 @@ public class Exercicio3 {
 	public static void main(String[] args) {
 			Scanner scanner = new Scanner(System.in);
 			
+			System.out.println("Digite um numero: ");
+			Double numero = scanner.nextDouble();
 			
 			
+			if (numero % 2==0) {
+				System.out.println("Numero Par");
+				
+			} else
+			{ 
+				System.out.println ("Numero Impar");
+				
+			}
 			
+		
+			
+			scanner.close();
 		}
+            
 	
 }
